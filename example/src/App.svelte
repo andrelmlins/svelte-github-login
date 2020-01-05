d81b74a25730e0348dabe92bab55bd2afb3076c6
André Lins <andrelucas01@hotmail.com>
2020-01-05 15:06:46 -0300

    Improvements in example

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,5 +1,8 @@
 <script>
   import SvelteGithubLogin from "svelte-github-login";
+
+  let clientId = "";
+  let status = "";
 </script>
 
 <style>
@@ -22,19 +25,65 @@
     color: white;
     margin: 10px 0px;
   }
+
+  p {
+    color: white;
+    margin: 10px 0px;
+  }
+
+  input {
+    width: auto;
+    flex: 1;
+    margin-right: 15px;
+  }
+
+  .input-form {
+    width: 450px;
+    max-width: 100%;
+    padding: 20px;
+    display: inline-flex;
+    box-sizing: border-box;
+    margin-top: 40px;
+  }
+
+  .success {
+    color: #1b5e20;
+  }
+
+  .failure {
+    color: #b71c1c;
+  }
+
+  .waiting {
+    color: #ffd600;
+  }
 </style>
 
 <main>
   <h1>Svelte Github Login</h1>
   <h4>Github Login Component to Svelte</h4>
-  <ul>
+  <div class="input-form">
+    <input
+      id="clientId"
+      type="text"
+      value={clientId}
+      placeholder="Client ID for GitHub OAuth application "
+      on:change={e => (clientId = e.target.value)} />
     <SvelteGithubLogin
-      clientId="XXX"
+      {clientId}
       redirectUri="http://localhost:5000/"
-      on:success={params => console.log(params)}
-      on:error={error => console.log(error)}
+      on:success={params => (status = 'Success')}
+      on:error={error => (status = 'Failure')}
+      on:request={() => (status = 'Waiting')}
       let:onLogin>
       <button on:click={onLogin}>Github Login</button>
     </SvelteGithubLogin>
-  </ul>
+  </div>
+  {#if status}
+    <p>
+      Status:
+      <span class={status.toLowerCase()}>{status}</span>
+    </p>
+  {/if}
+  {status}
 </main>
