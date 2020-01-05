fa0fb8db7dd5b3e7cb569cbeca04f01275b613c5
André Lins <andrelucas01@hotmail.com>
2020-01-05 10:37:52 -0300

    Creating githubLogin

---
+++ src/index.svelte
@@ -0,0 +1,59 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let clientId;
+  export let scope;
+  export let redirectUri;
+
+  const dispatch = createEventDispatcher();
+  const urlGithub = "https://github.com/login/oauth/authorize";
+  let popupWindow;
+
+  const convertQueryParams = url => {
+    const query = url.substr(1);
+    const result = {};
+
+    query.split("&").forEach(param => {
+      const item = param.split("=");
+      result[item[0]] = decodeURIComponent(item[1]);
+    });
+
+    dispatch("request");
+
+    return result;
+  };
+
+  // ReactGithubLogin based
+  const poll = () => {
+    window.setInterval(() => {
+      try {
+        if (!popupWindow || popupWindow.closed !== false) {
+          popupWindow.close();
+          dispatch("failure", new Error("The popup was closed"));
+          return;
+        }
+
+        if (
+          popupWindow.location.href === urlGithub ||
+          popupWindow.location.pathname === "blank"
+        ) {
+          return;
+        }
+
+        dispatch("success", convertQueryParams(popupWindow.location.search));
+        popupWindow.close();
+      } catch (error) {
+        dispatch("failure", error);
+      }
+    }, 500);
+  };
+
+  const onLogin = () => {
+    const urlParams = `client_id=${clientId}&scope=${scope}&redirectUri=${redirectUri}`;
+    popupWindow = window.open(`${urlGithub}?${urlParams}`, "github-oauth", "");
+
+    poll();
+  };
+</script>
+
+<slot {onLogin} />
